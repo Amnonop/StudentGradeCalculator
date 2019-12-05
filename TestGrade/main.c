04112c9ce7705e1d6f9770db45d7d0f53cb2720e
Amnon Ophir <ophiramnon@gmail.com>
2019-12-05 18:13:50 +0200

    Bugged - HW Thread

--- TestGrade/main.c
+++ TestGrade/main.c
@@ -20,7 +20,16 @@
 static int midterm_grade = 0;
 static int exam_grade = 0;
 static int hw_grades[NUM_OF_HW] = { 0 };
-
+//static int hw_grades[NUM_OF_HW] = { 0 };
+HANDLE p_thread_handles[NUM_THREADS];
+DWORD p_thread_ids[NUM_THREADS];
+DWORD wait_code;
+BOOL ret_val;
+size_t i;
+char* hw_file_names[NUM_OF_HW] =
+{ "ex01.txt","ex02.txt","ex03.txt",
+"ex04.txt", "ex05.txt", "ex06.txt",
+"ex07.txt", "ex08.txt", "ex09.txt", "ex10.txt" };
 
 /*declerations*/
 int getGradeFromFile(char* filename);
@@ -31,6 +40,7 @@ int getExamGrade(char* grades_directory);
 int calculateFinalGrade(float hw_grade, int midterm_grade, int exam_grade);
 DWORD WINAPI midtermGradeThread(LPVOID lpParam);
 DWORD WINAPI getExamGradeThread(LPVOID lpParam);
+DWORD WINAPI hwGradeThread(LPVOID lpParam);
 
 HANDLE createThreadSimple(LPTHREAD_START_ROUTINE p_start_routine,
 	LPVOID p_thread_parameters,
@@ -43,14 +53,7 @@ int main()
 	/*MIDTERM*/
 	/*FINAL A*/
 	/*FINAL B*/
-	HANDLE p_thread_handles[NUM_THREADS];
-	DWORD p_thread_ids[NUM_THREADS];
-	DWORD wait_code;
-	BOOL ret_val;
-	size_t i;
-
-	// Create two threads, each thread performs on task.
-
+	// Create two threads: Midterm , Exam A,B
 	p_thread_handles[0] = createThreadSimple(midtermGradeThread, grades_directory, &p_thread_ids[0]);
 	p_thread_handles[1] = createThreadSimple(getExamGradeThread, grades_directory, &p_thread_ids[1]);
 	//for (size_t i = 2; i < NUM_THREADS; i++)
@@ -70,23 +73,16 @@ int main()
 
 float getHomeWorkGrade(char* grades_directory)
 {
-	int hw_grades[NUM_OF_HW] = { 0 };
 	int i = 0;
-	char* hw_file_names[NUM_OF_HW] =
-	{ "ex01.txt","ex02.txt","ex03.txt",
-	"ex04.txt", "ex05.txt", "ex06.txt",
-	"ex07.txt", "ex08.txt", "ex09.txt", "ex10.txt" };
 	char* curr_file_path;
-
 	int directory_path_length = strlen(grades_directory);
 	int filename_length = directory_path_length + 1 + HW_FILENAME_LENGTH + 1;
-
 	for (int i = 0; i < NUM_OF_HW; i++)
 	{
 		curr_file_path = (char*)malloc(sizeof(char)*filename_length);
 		sprintf_s(curr_file_path, filename_length, "%s\\%s", grades_directory, hw_file_names[i]);
-
-		hw_grades[i] = getGradeFromFile(curr_file_path);
+		p_thread_handles[i+2] = (int)createThreadSimple(getGradeFromFile, grades_directory, &p_thread_ids[i+2]);
+		//hw_grades[i] = getGradeFromFile(curr_file_path);
 		free(curr_file_path);
 	}
 
@@ -138,6 +134,15 @@ HANDLE createThreadSimple(LPTHREAD_START_ROUTINE p_start_routine,
 	return thread_handle;
 }
 
+DWORD WINAPI hwGradeThread(LPVOID lpParam)
+{
+	char* grades_directory;
+	int curr_hw_grade = 0;
+	grades_directory = (char*)lpParam;
+	curr_hw_grade = getGradeFromFile(grades_directory);//GLOBAL
+	return (curr_hw_grade);
+}
+
 DWORD WINAPI midtermGradeThread(LPVOID lpParam)
 {
 	char* grades_directory;
@@ -151,7 +156,6 @@ DWORD WINAPI getExamGradeThread(LPVOID lpParam)
 	grades_directory = (char*)lpParam;
 	exam_grade = getExamGrade(grades_directory);//GLOBAL
 }
-
 
 
 int getMidtermGrade(char* grades_directory)
@@ -197,11 +201,31 @@ int getExamGrade(char* grades_directory)
 		exam_grade = moedA_grade;
 	if (exam_grade < FAIL_THRESHOLD)
 		exam_grade = 0;
-	return exam_grade;
+	return (exam_grade);
 }
 
 int getGradeFromFile(char* filename)
 {
+	int sub_grade = 0;
+	FILE *fp;
+	errno_t error;
+	error = fopen_s(&fp, filename, "r");
+
+	if (error != 0)
+		printf("An error occured while openning file %s for writing.", filename);
+
+	else if (fp)
+	{
+		fscanf_s(fp, "%d", &sub_grade);
+		fclose(fp);
+	}
+
+	return sub_grade;
+}
+
+int getGradeFromFile2(char* filename)
+{
+	/*Ecactly the same as above but for the HWThread*/
 	int sub_grade = 0;
 	FILE *fp;
 	errno_t error;
