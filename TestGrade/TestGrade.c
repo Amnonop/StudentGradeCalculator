01f052c7e35cc0292e1f35c92f43f999651b1e83
yuval <feldmanyuval@gmail.com>
2019-12-06 18:09:09 +0200

    WIP Trying to fix hw grade threading problem

--- TestGrade/TestGrade.c
+++ TestGrade/TestGrade.c
@@ -43,7 +43,7 @@ EXIT_CODE updateHWGrade(int hw_id, int hw_grade, HANDLE hw_mutex_handle);
 
 EXIT_CODE readGradeFromFile(const char *grades_directory, const char *grade_filename, int *grade);
 
-EXIT_CODE createHWMutex(HANDLE **mutex_handle);
+EXIT_CODE createHWMutex(HANDLE mutex_handle);
 HANDLE createMutexSimple(LPCTSTR mutex_name);
 
 typedef struct _hw_thread_params
@@ -59,10 +59,11 @@ int calculateGrade(char* grades_directory)
 	/*MIDTERM*/
 	/*FINAL A*/
 	/*FINAL B*/
+	char hw_mutex_name[] = "hw_mutex";
 	HANDLE hw_mutex_handle;
 	HANDLE p_thread_handles[NUM_THREADS];
 	DWORD p_thread_ids[NUM_THREADS];
-	hw_thread_params thread_params;
+	hw_thread_params thread_params[NUM_OF_HW];
 	DWORD wait_code;
 	BOOL ret_val;
 	EXIT_CODE exit_code;
@@ -70,9 +71,13 @@ int calculateGrade(char* grades_directory)
 	int hw_id = 0;
 
 	// Create mutex
-	exit_code = createHWMutex(&hw_mutex_handle);
-	if (exit_code != 0)
-		return exit_code;
+	hw_mutex_handle = createMutexSimple(hw_mutex_name);
+	if (hw_mutex_handle == NULL)
+	{
+		printf("Error creating HW mutex: %d\n", GetLastError());
+		CloseHandle(hw_mutex_handle);
+		return TG_MUTEX_CREATE_FAILED;
+	}
 
 	// Create two threads: Midterm , Exam A,B
 	p_thread_handles[0] = createThreadSimple(midtermGradeThread, grades_directory, &p_thread_ids[0]);
@@ -82,16 +87,20 @@ int calculateGrade(char* grades_directory)
 	for (i = 2; i < NUM_THREADS; i++)
 	{
 		// Create thread parameters
-		thread_params.grades_directory = grades_directory;
-		thread_params.hw_id = hw_id;
-		thread_params.hw_mutex_handle = hw_mutex_handle;
+		thread_params[hw_id].grades_directory = grades_directory;
+		thread_params[hw_id].hw_id = hw_id;
+		thread_params[hw_id].hw_mutex_handle = hw_mutex_handle;
 		hw_id++;
 		
-		p_thread_handles[i] = createThreadSimple(hwGradeThread, &thread_params, &p_thread_ids[i]);
+		p_thread_handles[i] = createThreadSimple(hwGradeThread, &(thread_params[hw_id]), &p_thread_ids[i]);
+		if (p_thread_handles[i] == NULL)
+		{
+			return TG_THREAD_CREATE_FAILED;
+		}
 	}
 
 	// Wait for all threads to terminate
-	wait_code = WaitForMultipleObjects(2, p_thread_handles, true, INFINITE);
+	wait_code = WaitForMultipleObjects(NUM_THREADS, p_thread_handles, true, INFINITE);
 	if ((wait_code == WAIT_TIMEOUT) || (wait_code == WAIT_FAILED))
 	{
 		printf("Error waiting for Midterm thread.\n");
@@ -127,7 +136,7 @@ int calculateGrade(char* grades_directory)
 	return 0;
 }
 
-EXIT_CODE createHWMutex(HANDLE **mutex_handle)
+EXIT_CODE createHWMutex(HANDLE mutex_handle)
 {
 	char hw_mutex_name[] = "hw_mutex";
 
@@ -180,17 +189,16 @@ float getHomeWorkGrade(char* grades_directory)
 DWORD WINAPI hwGradeThread(LPVOID lpParam)
 {
 	hw_thread_params *thread_params;
-	EXIT_CODE EXIT_CODE;
-	char* grades_directory;
-	int curr_hw_grade = 0;
+	EXIT_CODE exit_code;
 
+	Sleep(10);
 	if (!lpParam)
-		return ERROR_LPPARAM_NULL;
+		return TG_THREAD_PARAMS_NULL;
 
 	thread_params = (hw_thread_params*)lpParam;
 
-	EXIT_CODE = getHomeworkGrade(thread_params->grades_directory, thread_params->hw_id, thread_params->hw_mutex_handle);
-	return EXIT_CODE;
+	exit_code = getHomeworkGrade(thread_params->grades_directory, thread_params->hw_id, thread_params->hw_mutex_handle);
+	return exit_code;
 }
 
 EXIT_CODE getHomeworkGrade(char* grades_directory, int hw_id, HANDLE hw_mutex_handle)
@@ -217,7 +225,7 @@ EXIT_CODE getHomeworkGrade(char* grades_directory, int hw_id, HANDLE hw_mutex_ha
 
 	// This section should be locked
 	exit_code = updateHWGrade(hw_id, hw_grade, hw_mutex_handle);
-	if (exit_code != 0)
+		if (exit_code != 0)
 		return exit_code;
 
 	return 0;
