3024bbf929108da89340805eea570a73908e3ead
Yuval Feldman <feldmanyuval@gmail.com>
2019-12-07 13:53:40 +0200

    Added command line args handling
    The main function is getting the grades directory from the command line.

--- TestGrade/main.c
+++ TestGrade/main.c
@@ -1,10 +1,26 @@
+#include <string.h> 
+#include "Commons.h"
 #include "TestGrade.h"
 
-int main()
-{
-	char* grades_directory = "grades_123456789";
-	
-	calculateGrade(grades_directory);
+#define NUM_OF_ARGC 2
+#define GRADE_DIRECTORY_ARG_INDEX 1
 
-	return 0;
+int main(int argc, char *argv[])
+{
+	int grades_directory_length = 0;
+	char *grades_directory;
+	EXIT_CODE exit_code;
+
+	if (argc < NUM_OF_ARGC)
+		return TG_NOT_ENOUGH_COMMAND_LINE_ARGUMENTS;
+
+	grades_directory_length = strlen(argv[GRADE_DIRECTORY_ARG_INDEX]) + 1;
+	grades_directory = (char*)malloc(sizeof(char)*grades_directory_length);
+	strcpy_s(grades_directory, grades_directory_length, argv[GRADE_DIRECTORY_ARG_INDEX]);
+	
+	exit_code = calculateGrade(grades_directory);
+
+	free(grades_directory);
+
+	return exit_code;
 }
